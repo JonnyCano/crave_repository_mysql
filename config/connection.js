3b9e5851e94d8d02fde798e30973d4248097fd48
vang007 <84373534+vang007@users.noreply.github.com>
2021-09-06 16:08:34 -0500

    updated index.handlebars

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+  
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our database, pass in your MySQL information for the username and password
+//const sequelize = new Sequelize (
+ // 'nameof_db', 'username', 'password, {
+ //   host: 'localhost',
+ //   dialect: 'mysql',
+ //   port: 3306
+//});
+
+
+
+
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,53 @@
+
+const express = require('express');
+const mysql2 = require('mysql2');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+// get access to the array:
+const members = require('./Members');
+
+const app = express();
+
+// Init middleware
+// app.use(logger);
+
+// Handlebars Middleware:
+//set theview engine to handlebars pass in the exphbs
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+//setting the view engine
+app.set('view engine', 'handlebars');
+
+
+// Body Parser Middleware
+app.use(express.json());
+// handle url encoding data
+app.use(express.urlencoded({ extended: false }));
+
+// connect to the database
+
+// Homepage Route:
+app.get('/', (req, res) => {
+    res.render('index')
+    title: 'Animal Companion App',
+    members
+});
+
+// abput
+
+app.get('/about', (req, res) => {
+    res.render('about')
+});
+
+// route to the users pet profile
+
+// Set static folder
+// hint: move after the Homepage route to see another render of the server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// members api routes
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = 3001
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
