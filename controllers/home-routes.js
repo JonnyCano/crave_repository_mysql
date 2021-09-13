7cda97d44ef41d43ace01b1d8cc928ed032c4354
Jonathan Cano <jcano528@gmail.com>
2021-09-13 05:30:34 -0500

    I lovez you guys...

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,19 @@
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Owner } = require('../models');
 const router = require('express').Router();
 // Homepage Route:
 router.get('/', (req, res) => {
-  res.render('homepage');
-});
+  Owner.findOne({
+    where: {
+      id: req.query.id
+    }
+  })
+  .then(owner => {
+    res.render('homepage', {
+      fname: owner.fname.toString()
+    })
+  })
+  });
 
 router.get('/register', (req, res) => {
   res.render('register');
